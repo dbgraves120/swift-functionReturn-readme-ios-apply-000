622a83467dfbdac341abffb30cdc53886fc36515
David Graves <dbgraves120@gmail.com>
2016-12-10 21:03:30 -0500

    Completed

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -1,45 +1,51 @@
-func printAge(name: String) {
+func printAge(name: String)
+{
     print("\(name) is 29")
 }
 
-printAge("Adam")
+printAge(name: "Adam")
 
 
-func happyBirthday(age: Int) {
+func happyBirthday(age: Int)
+{
     print("Happy birthday #\(age)!")
 }
 
 
-func getAge(name: String) -> Int {
+func getAge(name: String) -> Int
+{
     return 29
 }
 
 // Uncomment to see the error
 /*
-func badGetAge(name: String) -> Int {
+func badGetAge(name: String) -> Int
+{
     print("\(name) is 29")
 }
 */
 
 
 let friend = "Emily"
-var friendAge = getAge(friend)
-happyBirthday(friendAge)
+var friendAge = getAge(name: friend)
+happyBirthday(age: friendAge)
 
 
-func birthdayGreeting(age: Int) -> String {
+func birthdayGreeting(age: Int) -> String
+{
     return "Happy birthday #\(age)!"
 }
 
-var greeting = birthdayGreeting(30)
+var greeting = birthdayGreeting(age: 30)
 print(greeting)
 
 
-func getAgeAndCongratulate(name: String) -> Int {
+func getAgeAndCongratulate(name: String) -> Int
+{
     let age = 29
     print("Happy \(age)th birthday, \(name)!")
     return age
 }
 
 let friend2 = "Pete"
-var friend2Age = getAgeAndCongratulate(friend2)
+var friend2Age = getAgeAndCongratulate(name: friend2)
